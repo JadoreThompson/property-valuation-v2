5942f713433f77191b100f1c12088f00143e71b9
LucioJamieAI <47329590+LucioJamieAI@users.noreply.github.com>
2024-08-29 00:06:39 +0100

    Gitignore

--- dojo.py
+++ dojo.py
@@ -1,3 +1,40 @@
-while True:
-    prompt = input("You: ")
-    print("Bot: ")
+import aiohttp
+import os
+from dotenv import load_dotenv
+
+
+def run_dojo():
+    while True:
+        prompt = input("You: ")
+        print("Bot: ")
+
+
+load_dotenv(".env")
+
+url = "https://places.googleapis.com/v1/places:searchText"
+places_api_key = os.getenv('GOOGLE_PLACES_API_KEY')
+header = {
+    'X-Goog-Api-Key': places_api_key,
+    'X-Goog-FieldMask': 'places.displayName,places.name,places.id,places.websiteUri,places.nationalPhoneNumber,places.rating,nextPageToken',
+    'Content-Type': 'application/json'
+}
+payload = {
+    'textQuery': "schools",
+    'locationBias': {
+        'circle': {
+            'center': {'latitude': 51.5964, 'longitude': 0.0349},
+            'radius': 50000.0
+        }
+    },
+    'pageSize': 100
+}
+
+
+async with aiohttp.ClientSession() as session:
+    async with session.post(url, headers=header, json=payload) as rsp:
+        print(rsp)
+
+
+
+if __name__ == "__main__":
+    run_dojo()
--- scripts/rightmove.py
+++ scripts/rightmove.py
@@ -1,11 +1,56 @@
+import re
 import asyncio
 from playwright.async_api import async_playwright
 
 
+# ---------------------------
+# Scraping the landing page
+# --------------------------
+def get_listing(lines):
+    locations = []
+
+    for line in lines:
+        if re.match(r'^(?:(?:\d+|Flat\s+\d+|Apartment\s+\d+),\s+[\w\s]+,)', line):
+            locations.append(line)
+    return locations
+
+
+async def handle_page_listings(all_page_listings):
+    for _, item in enumerate(all_page_listings, 1):
+        page_text = await item.inner_text()
+        starting_point = page_text.find("Page desc")
+        lines = page_text[starting_point + len("Page desc"):].strip().split("\n")
+        listings = get_listing(lines)
+    return listings
+
+
+async def get_page_listings(page):
+    try:
+        all_page_listings = await page.locator(".results").all()
+        if all_page_listings:
+            page_listings = await handle_page_listings(all_page_listings)
+            for listing in page_listings:
+                await scrape_individual_page_listing(listing, page)
+    except Exception as e:
+        print(e)
+
+
+async def scrape_individual_page_listing(listing, page):
+    try:
+        await page.locator(f"a:has-text('{listing}')").click(timeout=3000)
+    except Exception as e:
+        print(f"Couldn't find link for {listing}: {e}")
+        return
+
+
+# ------------------------------
+#                    Run
+# ------------------------------
 async def launch_scraper(page, location):
-    url = f"https://www.rightmove.co.uk/house-prices/{location}.html?soldIn=2&radius=0.25&page=1"
+    url = f"https://www.rightmove.co.uk/house-prices/{location}.html?soldIn=2&page=1"
     await page.goto(url)
     await page.locator("button:has-text('Accept all')").click()
+    await get_page_listings(page)
     await asyncio.sleep(10000)
 
 

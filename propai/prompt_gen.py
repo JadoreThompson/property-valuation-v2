639ab8d67cdd7d611ff801912024cc49829ff1ff
LucioJamieAI <47329590+LucioJamieAI@users.noreply.github.com>
2024-09-05 00:13:28 +0100

    fixed double outgoing card bug

--- propai/prompt_gen.py
+++ propai/prompt_gen.py
@@ -5,13 +5,8 @@ from datetime import datetime, timedelta
 
 from operator import itemgetter
 from langchain_community.utilities import SQLDatabase
-from langchain.chains import create_sql_query_chain
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import PromptTemplate
-from langchain_core.runnables import RunnablePassthrough
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_community.tools.sql_database.tool import QuerySQLDataBaseTool
-
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.agent_toolkits import create_sql_agent
 from langchain_core.prompts import (
     ChatPromptTemplate,
@@ -21,11 +16,7 @@ from langchain_core.prompts import (
     SystemMessagePromptTemplate,
 )
 
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
-
 from dojo import ROOT_DIR
-from db_connection import get_db_conn
 
 
 load_dotenv(os.path.join(ROOT_DIR, ".env"))
@@ -157,5 +148,4 @@ llm = ChatGoogleGenerativeAI(
 
 sql_agent = create_sql_agent(llm=llm, db=db, agent_type="tool-calling", verbose=True, prompt=full_prompt)
 
-# rsp = sql_agent.invoke({"input": "what's change in house prices between last year and this year for kensington"})
-# print(rsp["output"])
+
